53a0588849c9a3976221cfa438acb3d888f6ecd7
l3njo <plenjo15@gmail.com>
2019-06-16 23:14:49 +0300

    Database connection
    Connect to postgresql db using GORM

---
+++ models/database.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var db *gorm.DB
+
+func init() {
+
+	e := godotenv.Load()
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	username := os.Getenv("db_user")
+	password := os.Getenv("db_pass")
+	dbName := os.Getenv("db_name")
+	dbHost := os.Getenv("db_host")
+
+	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+	fmt.Println(dbURI)
+
+	db, err := gorm.Open("postgres", dbURI)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	db.Debug().AutoMigrate(&Account{}, &Person{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
