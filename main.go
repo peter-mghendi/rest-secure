04cf6a7240e65b93db7d18bdefd3caeea4015eca
l3njo <plenjo15@gmail.com>
2019-06-16 23:17:55 +0300

    Create app entry point

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"go-contacts/app"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
+
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	fmt.Printf("Serving on localhost:%v/api", port)
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
